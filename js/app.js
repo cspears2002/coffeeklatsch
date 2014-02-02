a9b1e77ba6cb06807329375c1d78e2024325b715
cspears2002 <cspears2002@yahoo.com>
2014-02-01 16:47:29 -0800

    Used fixtures to seed the data.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 App = Ember.Application.create();
 
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
 // Routes
 App.Router.map(function() {
   this.resource('coffee', { path: '/' });
@@ -10,6 +12,24 @@ App.IndexRoute = Ember.Route.extend({
 
 // Models
 App.Coffee = DS.Model.extend({
-  title: DS.attr('string'),
-  isCompleted: DS.attr('boolean')
+  bean: DS.attr('string'),
+  price: DS.attr('string')
 });
+
+App.Coffee.Fixtures = [
+  {
+    id: 1,
+    bean: 'Espresso Torro',
+    price: '$17.75'
+  },
+  {
+    id: 2,
+    bean: 'Sulawesi Toarco AA Tana Toraja',
+    price: '$18.75'
+  },
+  {
+    id: 3,
+    bean: '100% Estate Kona',
+    price: '$39.75'
+  }, 
+];
